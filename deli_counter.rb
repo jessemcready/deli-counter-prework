abdda4e5c5728cacd0d4f70ef665fac4c7422f98
Jesse McReady <jessemcready@gmail.com>
2018-06-21 01:15:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    people = 0...array.size
+    current_line = "The line is currently: "
+    for person in people do
+      current_line += "#{person + 1}. #{array[person]} "
+    end
+    puts current_line[0...-1]
+  end
+end
+
+def take_a_number(line, person)
+  
+end
